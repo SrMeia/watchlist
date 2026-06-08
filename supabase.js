4c65f8a075728270fe36849ce55bb2834203acab
SrMeia <gomcalvesdiasleonardo@gmail.com>
2026-06-07 23:09:06 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://hbcsiyqnefiszabtkbgo.supabase.co/";
-const SUPABASE_KEY = "sb_publishable_Ipaqhir9-tGHvL9NdXKB6Q_rZRBQW0O";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiY3NpeXFuZWZpc3phYnRrYmdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2OTc2NjIsImV4cCI6MjA5NjI3MzY2Mn0.Jgvv6I-Wn83JfF8RzdZHRLyOFqHwY3-JvmJRQi2tXuM";
 
 const client = supabase.createClient(
   SUPABASE_URL,
